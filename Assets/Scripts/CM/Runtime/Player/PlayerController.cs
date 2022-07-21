affa3d65bd32574b281f3eb598a7d0ef592a0e59
Can Mujde <can.mujde@icloud.com>
2022-07-22 01:28:12 +0300

    player controller update

--- Assets/Scripts/CM/Runtime/Player/PlayerController.cs
+++ Assets/Scripts/CM/Runtime/Player/PlayerController.cs
@@ -6,77 +6,99 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Animator animator;
-    [SerializeField] private float _acceleration = 80;
-    [SerializeField] private float _maxVelocity = 10;
     [SerializeField] private float _rotationSpeed = 450;
-    private Plane _groundPlane = new(Vector3.up, Vector3.zero);
+    public LayerMask IgnoreMe;
+    public Transform turret;
     private Camera _cam;
-    
+
+    public GameObject ball;
     private Vector3 _input;
     private Rigidbody _rb;
 
-    public PhotonView View { get; private set; }
+    public PhotonView View;
     private static readonly int IsRunningAnimatorKey = Animator.StringToHash("isRunning");
-    
-    
-    
-    private void Awake() {
+
+
+    private void Awake()
+    {
         _rb = GetComponent<Rigidbody>();
         _cam = Camera.main;
     }
+
     private void Start()
     {
         View = GetComponent<PhotonView>();
-        
-        if (View.IsMine) CameraFollower.Instance.SetTarget(transform);
-    }
-    private void Update() {
-        
-        if (!View.IsMine) return;
-        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        animator.SetBool(IsRunningAnimatorKey, _input != Vector3.zero);
+
+
+        if (View.IsMine)
+        {
+            gameObject.name = "Mine";
+            CameraFollower.Instance.SetTarget(transform);
+        }
     }
 
-    private void FixedUpdate() {
+    private void Update()
+    {
         if (!View.IsMine) return;
-        HandleMovement();
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        Ball ball = PhotonNetwork.Instantiate("Ball", turret.position, Quaternion.identity).GetComponent<Ball>();
+        ball.Initialize(-lastDirection);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!View.IsMine) return;
         HandleRotation();
     }
 
     #region Movement
 
-
-
-    private void HandleMovement() {
-        _rb.velocity += _input.normalized * (_acceleration * Time.deltaTime);
-        _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _maxVelocity);
+    private Vector3 MouseHitPoint()
+    {
+        var ray = _cam.ScreenPointToRay(Input.mousePosition);
+        return !Physics.Raycast(ray, out var hit, 1000f, ~IgnoreMe) ? Vector3.zero : hit.point;
     }
+
+    private Vector3 Direction()
+    {
+        var dir = MouseHitPoint() == Vector3.zero ? lastDirection :  MouseHitPoint() - _rb.position;
+        dir.y = 0;
+        return dir;
+    }
+    
 
     #endregion
 
     #region Rotation
 
-    
+    private Vector3 lastDirection;
+    private void HandleRotation()
+    {
+        lastDirection = Direction() == Vector3.zero ? lastDirection : Direction();
+        var rotation = Quaternion.LookRotation(lastDirection);
+        turret.rotation = Quaternion.Lerp(turret.rotation, rotation, Time.deltaTime * _rotationSpeed);
+    }
 
-    private void HandleRotation() {
-        var ray = _cam.ScreenPointToRay(Input.mousePosition);
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if (!PhotonNetwork.IsMasterClient) return;
+        // if (!View.IsMine) return;
 
-        if (_groundPlane.Raycast(ray, out var enter)) {
-            var hitPoint = ray.GetPoint(enter);
-
-            
-            var lookPos = hitPoint - transform.position;
-            lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * _rotationSpeed);
-            
-            
-            //
-            // var dir = hitPoint - transform.position;
-            // var rot = Quaternion.LookRotation(dir);
-            //
-            // transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _rotationSpeed * Time.deltaTime);
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            var enemy = collision.gameObject.GetComponent<Enemy>();
+            if (enemy.Destroyed) return;
+            enemy.Destroyed = true;
+            NetworkManager.Instance.KillEnemy(enemy.gameObject);
         }
     }
 
